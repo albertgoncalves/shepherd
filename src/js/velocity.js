731aeeb988d7d809cf7fc5787c2edea20cdd2908
albertgoncalves <goncalves.albert@gmail.com>
2019-11-30 18:04:22 -0500

    special velocity!

--- src/js/velocity.js
+++ src/js/velocity.js
@@ -12,12 +12,14 @@ function createCircle(x) {
         x: x,
         y: HALF_HEIGHT,
         radius: RADIUS,
-        speed: 0,
-        fillStyle: "hsla(0, 0%, 35%, 0.75)",
+        speedRegular: 0,
+        speedSpecial: 0,
+        color: "hsl(0, 0%, 45%)",
     };
 }
 
-var N = 75;
+var N = 250;
+var M = N - 1;
 var CIRCLES = new Array(N);
 for (var i = 0; i < N; i++) {
     CIRCLES[i] = createCircle(CANVAS.width * ((i + 0.5) / N));
@@ -25,32 +27,47 @@ for (var i = 0; i < N; i++) {
 
 var LOWER = CANVAS.height / 7;
 var UPPER = CANVAS.height - LOWER;
-var SCALE = 25;
+var SCALE = 10;
 var MAX_SPEED = 1;
 var MIN_SPEED = -1;
+var MAGNITUDE = 1 / (N * 3);
+var CENTER = MAGNITUDE / 2;
 
 function loop() {
     CTX.clearRect(0, 0, CANVAS.width, CANVAS.height);
     for (var i = 0; i < N; i++) {
-        CIRCLES[i].speed += (Math.random() * 0.25) - 0.125;
-        CIRCLES[i].y += CIRCLES[i].speed;
+        CIRCLES[i].speedRegular += (Math.random() * MAGNITUDE) - CENTER;
+        if (CIRCLES[i].speedRegular < MIN_SPEED) {
+            CIRCLES[i].speedRegular = MIN_SPEED;
+        } else if (MAX_SPEED < CIRCLES[i].speedRegular) {
+            CIRCLES[i].speedRegular = MAX_SPEED;
+        }
+        if (CIRCLES[i].speedSpecial < 0) {
+            for (var j = 0; j < i; j++) {
+                CIRCLES[i].speedSpecial += CIRCLES[j].speedRegular;
+            }
+        } else {
+            for (var k = i + 1; k < N; k++) {
+                CIRCLES[i].speedSpecial += CIRCLES[k].speedRegular;
+            }
+        }
+        CIRCLES[i].y += CIRCLES[i].speedSpecial;
         if (CIRCLES[i].y < LOWER) {
             CIRCLES[i].y = LOWER;
         } else if (UPPER < CIRCLES[i].y) {
             CIRCLES[i].y = UPPER;
         }
-        if (CIRCLES[i].speed < MIN_SPEED) {
-            CIRCLES[i].speed = MIN_SPEED;
-        } else if (MAX_SPEED < CIRCLES[i].speed) {
-            CIRCLES[i].speed = MAX_SPEED;
-        }
+    }
+    for (var l = 1; l < M; l++) {
         CTX.beginPath();
-        CTX.arc(CIRCLES[i].x, CIRCLES[i].y, CIRCLES[i].radius, 0, PI_2);
-        CTX.fillStyle = CIRCLES[i].fillStyle;
+        CTX.arc(CIRCLES[l].x, CIRCLES[l].y, CIRCLES[l].radius, 0, PI_2);
+        CTX.fillStyle = CIRCLES[l].color;
         CTX.fill();
         CTX.beginPath();
-        CTX.moveTo(CIRCLES[i].x, CIRCLES[i].y);
-        CTX.lineTo(CIRCLES[i].x, CIRCLES[i].y + (CIRCLES[i].speed * SCALE));
+        CTX.moveTo(CIRCLES[l].x, CIRCLES[l].y);
+        CTX.lineTo(CIRCLES[l].x,
+                   CIRCLES[l].y + (CIRCLES[l].speedSpecial * SCALE));
+        CTX.strokeStyle = CIRCLES[l].color;
         CTX.stroke();
     }
     requestAnimationFrame(loop);
