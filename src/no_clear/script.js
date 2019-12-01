61338f462c4f504769b4edaa25bc273171f6c0ac
albertgoncalves <goncalves.albert@gmail.com>
2019-11-30 21:59:13 -0500

    implement `no_clear` and fix `$ fmt`

--- src/no_clear/script.js
+++ src/no_clear/script.js
@@ -4,7 +4,7 @@ var CANVAS = document.getElementById("canvas");
 var CTX = CANVAS.getContext("2d");
 
 var PI_2 = Math.PI * 2;
-var RADIUS = 3;
+var RADIUS = 0.25;
 var HALF_HEIGHT = CANVAS.height / 2;
 
 function createCircle(x) {
@@ -14,27 +14,29 @@ function createCircle(x) {
         radius: RADIUS,
         speedRegular: 0,
         speedSpecial: 0,
-        color: "hsl(0, 0%, 45%)",
+        color: "hsla(0, 0%, 35%, 0.1)",
     };
 }
 
-var N = 250;
-var M = N - 1;
+var N = CANVAS.width;
+var HIDDEN = 20;
+var M = N - HIDDEN;
 var CIRCLES = new Array(N);
 for (var i = 0; i < N; i++) {
     CIRCLES[i] = createCircle(CANVAS.width * ((i + 0.5) / N));
 }
 
-var LOWER = CANVAS.height / 7;
+var LOWER = CANVAS.height / 10;
 var UPPER = CANVAS.height - LOWER;
 var SCALE = 10;
 var MAX_SPEED = 1;
 var MIN_SPEED = MAX_SPEED * -1;
-var MAGNITUDE = 1 / (N * 3);
+var LOWER_BOUNCE = 0.0001;
+var UPPER_BOUNCE = LOWER_BOUNCE * -1;
+var MAGNITUDE = 1 / N;
 var CENTER = MAGNITUDE / 2;
 
 function loop() {
-    CTX.clearRect(0, 0, CANVAS.width, CANVAS.height);
     for (var i = 0; i < N; i++) {
         CIRCLES[i].speedRegular += (Math.random() * MAGNITUDE) - CENTER;
         if (CIRCLES[i].speedRegular < MIN_SPEED) {
@@ -54,21 +56,17 @@ function loop() {
         CIRCLES[i].y += CIRCLES[i].speedSpecial;
         if (CIRCLES[i].y < LOWER) {
             CIRCLES[i].y = LOWER;
+            CIRCLES[i].speedRegular = LOWER_BOUNCE;
         } else if (UPPER < CIRCLES[i].y) {
             CIRCLES[i].y = UPPER;
+            CIRCLES[i].speedRegular = UPPER_BOUNCE;
         }
     }
-    for (var l = 1; l < M; l++) {
+    for (var l = HIDDEN; l < M; l++) {
         CTX.beginPath();
         CTX.arc(CIRCLES[l].x, CIRCLES[l].y, CIRCLES[l].radius, 0, PI_2);
         CTX.fillStyle = CIRCLES[l].color;
         CTX.fill();
-        CTX.beginPath();
-        CTX.moveTo(CIRCLES[l].x, CIRCLES[l].y);
-        CTX.lineTo(CIRCLES[l].x,
-                   CIRCLES[l].y + (CIRCLES[l].speedSpecial * SCALE));
-        CTX.strokeStyle = CIRCLES[l].color;
-        CTX.stroke();
     }
     requestAnimationFrame(loop);
 }
