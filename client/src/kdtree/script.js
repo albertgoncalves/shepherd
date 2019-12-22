02f1f63f71a91fb2246e2974e34a2d3c8d249321
albertgoncalves <goncalves.albert@gmail.com>
2019-12-22 16:04:32 -0500

    implement nearestNeighbors()

--- client/src/kdtree/script.js
+++ client/src/kdtree/script.js
@@ -1,17 +1,19 @@
 "use strict";
 
 function distanceSquared(a, b) {
-    var x = a.x - b.x;
-    var y = a.y - b.y;
+    var x, y;
+    x = a.x - b.x;
+    y = a.y - b.y;
     return (x * x) + (y * y);
 }
 
 var CANVAS = document.getElementById("canvas");
 var CTX = CANVAS.getContext("2d");
-var COLOR = "hsl(0, 0%, 90%)";
+var WHITE = "hsl(0, 0%, 90%)";
+var BLUE = "hsl(200, 75%, 65%)";
+var RED = "hsl(0, 85%, 65%)";
 CTX.imageSmoothingEnabled = false;
-CTX.strokeStyle = COLOR;
-CTX.fillStyle = COLOR;
+CTX.strokeStyle = WHITE;
 CTX.lineWidth = 2;
 
 function randomPoint() {
@@ -22,10 +24,10 @@ function randomPoint() {
 }
 
 var PI_2 = Math.PI * 2;
-var RADIUS = 5;
-var D = 2;
+var RADIUS = 8;
 var N = 13;
 var POINTS = new Array(N);
+var POINT;
 
 function order(axis) {
     if (axis === 0) {
@@ -36,26 +38,61 @@ function order(axis) {
         return function(a, b) {
             return a.y - b.y;
         };
-    } else {
-        return undefined;
     }
 }
 
-function buildTree(points, depth) {
-    var n = points.length;
+function buildTree(points, axis) {
+    var n, median, next;
+    n = points.length;
     if (n <= 0) {
         return null;
     }
-    var axis = depth % D;
     points.sort(order(axis));
-    var m = Math.floor(n / 2);
-    var d = axis + 1;
+    median = Math.floor(n / 2);
+    next = axis === 1 ? 0 : 1;
     return {
-        point: points[m],
-        left: buildTree(points.slice(0, m), d),
-        right: buildTree(points.slice(m + 1), d),
+        point: points[median],
+        left: buildTree(points.slice(0, median), next),
+        right: buildTree(points.slice(median + 1), next),
         axis: axis,
     };
+}
+
+function branchDistance(point, a, b) {
+    if (a === null) {
+        return b;
+    } else if (b === null) {
+        return a;
+    }
+    if (distanceSquared(point, a) < distanceSquared(point, b)) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+function nearestNeighbor(tree, point) {
+    var pointAxis, treeAxis, nextBranch, oppositeBranch, best, delta;
+    if (tree === null) {
+        return null;
+    }
+    pointAxis = tree.axis === 0 ? point.x : point.y;
+    treeAxis = tree.axis === 0 ? tree.point.x : tree.point.y;
+    if (pointAxis < treeAxis) {
+        nextBranch = tree.left;
+        oppositeBranch = tree.right;
+    } else {
+        oppositeBranch = tree.left;
+        nextBranch = tree.right;
+    }
+    best =
+        branchDistance(point, nearestNeighbor(nextBranch, point), tree.point);
+    delta = pointAxis - treeAxis;
+    if ((delta * delta) < distanceSquared(point, best)) {
+        best = branchDistance(point, nearestNeighbor(oppositeBranch, point),
+                              best);
+    }
+    return best;
 }
 
 function drawTree(tree, xLower, xUpper, yLower, yUpper) {
@@ -79,32 +116,58 @@ var SCALE = MAGNITUDE / 2;
 var RESET = 360;
 var ELAPSED = RESET + 1;
 
+function drawCircle(x, y) {
+    CTX.moveTo(x + RADIUS, y);
+    CTX.arc(x, y, RADIUS, 0, PI_2);
+}
+
 function loop() {
-    var i, x, y;
+    var i, x, y, tree, neighbor;
     if (RESET < ELAPSED) {
         ELAPSED = 0;
+        POINT = randomPoint();
         for (i = 0; i < N; i++) {
             POINTS[i] = randomPoint();
         }
     } else {
         ELAPSED += 1;
     }
+    POINT.x += (Math.random() * MAGNITUDE) - SCALE;
+    POINT.y += (Math.random() * MAGNITUDE) - SCALE;
     for (i = 0; i < N; i++) {
         POINTS[i].x += (Math.random() * MAGNITUDE) - SCALE;
         POINTS[i].y += (Math.random() * MAGNITUDE) - SCALE;
     }
+    tree = buildTree(POINTS, 0);
+    neighbor = nearestNeighbor(tree, POINT);
     CTX.clearRect(0, 0, CANVAS.width, CANVAS.height);
-    CTX.beginPath();
-    drawTree(buildTree(POINTS, 0), 0, CANVAS.width, 0, CANVAS.height);
-    CTX.stroke();
-    CTX.beginPath();
-    for (i = 0; i < N; i++) {
-        x = POINTS[i].x;
-        y = POINTS[i].y;
-        CTX.moveTo(x + RADIUS, y);
-        CTX.arc(x, y, RADIUS, 0, PI_2);
+    {
+        CTX.beginPath();
+        drawTree(tree, 0, CANVAS.width, 0, CANVAS.height);
+        CTX.stroke();
     }
-    CTX.fill();
+    {
+        CTX.beginPath();
+        drawCircle(POINT.x, POINT.y);
+        CTX.fillStyle = BLUE;
+        CTX.fill();
+    }
+    {
+        CTX.beginPath();
+        drawCircle(neighbor.x, neighbor.y);
+        CTX.fillStyle = RED;
+        CTX.fill();
+    }
+    {
+        CTX.beginPath();
+        for (i = 0; i < N; i++) {
+            if (POINTS[i] !== neighbor) {
+                drawCircle(POINTS[i].x, POINTS[i].y);
+            }
+        }
+        CTX.fillStyle = WHITE;
+        CTX.fill();
+    }
     requestAnimationFrame(loop);
 }
 
