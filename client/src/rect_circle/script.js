0105e68bc707bbb0e007e95f1a657e9def4a1d89
albertgoncalves <goncalves.albert@gmail.com>
2019-12-24 16:28:02 -0500

    adjustments

--- client/src/rect_circle/script.js
+++ client/src/rect_circle/script.js
@@ -2,22 +2,30 @@
 
 var CANVAS = document.getElementById("canvas");
 var CTX = CANVAS.getContext("2d");
-var WHITE = "hsl(0, 0%, 90%)";
+var COLOR = "hsl(0, 0%, 35%)";
 CTX.imageSmoothingEnabled = false;
-CTX.strokeStyle = WHITE;
-CTX.fillStyle = WHITE;
-CTX.lineWidth = 3;
+CTX.strokeStyle = COLOR;
+CTX.fillStyle = COLOR;
+CTX.lineWidth = 4;
 
 var PI_2 = Math.PI * 2;
-var POINT_R = 7;
-var RECT_K = 500;
-var CIRCLE_R = RECT_K / 4;
-var CIRCLE_R_2 = CIRCLE_R * 2;
+var POINT_RADIUS = 7;
+var WIDTH = 300;
+var HEIGHT = 150;
+var CIRCLE_RADIUS = 100;
+var CIRCLE_RADIUS_2 = CIRCLE_RADIUS * 2;
 var RESET = 360;
 var ELAPSED = RESET + 1;
-var MAGNITUDE = 1.5;
+var MAGNITUDE = 0.5;
 var SCALE = MAGNITUDE / 2;
-var WIDTH, HEIGHT, RECTANGLE, CIRCLE, POINT;
+var RECTANGLE = {
+    width: WIDTH,
+    height: HEIGHT,
+};
+var CIRCLE = {
+    radius: CIRCLE_RADIUS,
+};
+var POINT;
 
 function nearest(rectangle, point) {
     return {
@@ -42,19 +50,12 @@ function intersect(rectangle, circle) {
 function loop() {
     if (RESET < ELAPSED) {
         ELAPSED = 0;
-        WIDTH = Math.random() * RECT_K;
-        HEIGHT = Math.random() * RECT_K;
-        RECTANGLE = {
-            x: Math.random() * (CANVAS.width - WIDTH),
-            y: Math.random() * (CANVAS.height - HEIGHT),
-            width: WIDTH,
-            height: HEIGHT,
-        };
-        CIRCLE = {
-            x: (Math.random() * (CANVAS.width - CIRCLE_R_2)) + CIRCLE_R,
-            y: (Math.random() * (CANVAS.height - CIRCLE_R_2)) + CIRCLE_R,
-            radius: CIRCLE_R,
-        };
+        RECTANGLE.x = Math.random() * (CANVAS.width - WIDTH);
+        RECTANGLE.y = Math.random() * (CANVAS.height - HEIGHT);
+        CIRCLE.x =
+            (Math.random() * (CANVAS.width - CIRCLE_RADIUS_2)) + CIRCLE_RADIUS;
+        CIRCLE.y = (Math.random() * (CANVAS.height - CIRCLE_RADIUS_2)) +
+            CIRCLE_RADIUS;
         POINT = nearest(RECTANGLE, CIRCLE);
     } else {
         ELAPSED += 1;
@@ -65,12 +66,12 @@ function loop() {
         POINT = nearest(RECTANGLE, CIRCLE);
     }
     CTX.clearRect(0, 0, CANVAS.width, CANVAS.height);
+    CTX.strokeRect(RECTANGLE.x, RECTANGLE.y, RECTANGLE.width,
+                   RECTANGLE.height);
     {
-        CTX.strokeRect(RECTANGLE.x, RECTANGLE.y, RECTANGLE.width,
-                       RECTANGLE.height);
         CTX.beginPath();
-        CTX.moveTo(CIRCLE.x + CIRCLE_R, CIRCLE.y);
-        CTX.arc(CIRCLE.x, CIRCLE.y, CIRCLE_R, 0, PI_2);
+        CTX.moveTo(CIRCLE.x + CIRCLE.radius, CIRCLE.y);
+        CTX.arc(CIRCLE.x, CIRCLE.y, CIRCLE.radius, 0, PI_2);
         CTX.stroke();
     }
     if (!intersect(RECTANGLE, CIRCLE)) {
@@ -87,10 +88,10 @@ function loop() {
         }
         {
             CTX.beginPath();
-            CTX.moveTo(POINT.x + POINT_R, POINT.y);
-            CTX.arc(POINT.x, POINT.y, POINT_R, 0, PI_2);
-            CTX.moveTo(intersection.x + POINT_R, intersection.y);
-            CTX.arc(intersection.x, intersection.y, POINT_R, 0, PI_2);
+            CTX.moveTo(POINT.x + POINT_RADIUS, POINT.y);
+            CTX.arc(POINT.x, POINT.y, POINT_RADIUS, 0, PI_2);
+            CTX.moveTo(intersection.x + POINT_RADIUS, intersection.y);
+            CTX.arc(intersection.x, intersection.y, POINT_RADIUS, 0, PI_2);
             CTX.fill();
         }
     }
