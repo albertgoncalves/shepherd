1dc5e38f398b372c11bbc00e697532d771de14ff
albertgoncalves <goncalves.albert@gmail.com>
2020-06-08 09:49:14 -0400

    parallel!

--- shell.nix
+++ shell.nix
@@ -2,8 +2,10 @@ with import <nixpkgs> {};
 llvmPackages_10.stdenv.mkDerivation {
     name = "_";
     buildInputs = [
+        glibcLocales
         htmlTidy
         nodejs
+        parallel
         shellcheck
     ];
     shellHook = ''
