d955c1f2b83ced586b6f2cfd717da492dcdd8437
Albert Goncalves <goncalves.albert@gmail.com>
2020-07-12 11:53:44 -0400

    re-factor

--- src/velocity/script.js
+++ src/velocity/script.js
@@ -7,58 +7,60 @@ var PI_2 = Math.PI * 2;
 var RADIUS = 3;
 var N = 300;
 var M = N - 1;
-var XS = new Float32Array(N);
-var YS = new Float32Array(N);
-var POINT_SPEED = new Float32Array(N);
-var AGGREGATE_SPEED = new Float32Array(N);
-var SCALE = 15;
-var MAGNITUDE = 1.1;
-var CENTER = MAGNITUDE / 2;
+var POINTS = {
+    x: new Float32Array(N),
+    y: new Float32Array(N),
+    speed: new Float32Array(N),
+    neighborSpeed: new Float32Array(N),
+};
+var L = 15.0;
+var SCALE = 1.1;
+var HALF_SCALE = SCALE / 2;
 var OFFSET = 0.3;
-var OFFSET_LOWER = CENTER * (1 - OFFSET);
-var OFFSET_UPPER = CENTER * (1 + OFFSET);
+var OFFSET_LOWER = HALF_SCALE * (1.0 - OFFSET);
+var OFFSET_UPPER = HALF_SCALE * (1.0 + OFFSET);
 
 function loop() {
     for (var i = 0; i < N; ++i) {
-        POINT_SPEED[i] += (Math.random() * MAGNITUDE) - CENTER;
-        var norm = 0;
-        if (AGGREGATE_SPEED[i] < 0) {
+        POINTS.speed[i] += (Math.random() * SCALE) - HALF_SCALE;
+        var norm = 0.0;
+        if (POINTS.neighborSpeed[i] < 0.0) {
             for (var j = 0; j < i; ++j) {
-                AGGREGATE_SPEED[i] += POINT_SPEED[j];
-                norm += 1;
+                POINTS.neighborSpeed[i] += POINTS.speed[j];
+                norm += 1.0;
             }
         } else {
             for (var j = i + 1; j < N; ++j) {
-                AGGREGATE_SPEED[i] += POINT_SPEED[j];
-                norm += 1;
+                POINTS.neighborSpeed[i] += POINTS.speed[j];
+                norm += 1.0;
             }
         }
-        YS[i] += AGGREGATE_SPEED[i] / norm;
-        if (YS[i] < LOWER) {
-            YS[i] = LOWER;
-            POINT_SPEED[i] = (Math.random() * MAGNITUDE) - OFFSET_LOWER;
-            AGGREGATE_SPEED[i] = 0;
-        } else if (UPPER < YS[i]) {
-            YS[i] = UPPER;
-            POINT_SPEED[i] = (Math.random() * MAGNITUDE) - OFFSET_UPPER;
-            AGGREGATE_SPEED[i] = 0;
+        POINTS.y[i] += POINTS.neighborSpeed[i] / norm;
+        if (POINTS.y[i] < LOWER) {
+            POINTS.y[i] = LOWER;
+            POINTS.speed[i] = (Math.random() * SCALE) - OFFSET_LOWER;
+            POINTS.neighborSpeed[i] = 0.0;
+        } else if (UPPER < POINTS.y[i]) {
+            POINTS.y[i] = UPPER;
+            POINTS.speed[i] = (Math.random() * SCALE) - OFFSET_UPPER;
+            POINTS.neighborSpeed[i] = 0.0;
         }
     }
     CTX.clearRect(0, 0, CANVAS.width, CANVAS.height);
     CTX.beginPath();
     for (var i = 1; i < M; ++i) {
-        var x = XS[i];
-        var y = YS[i];
+        var x = POINTS.x[i];
+        var y = POINTS.y[i];
         CTX.moveTo(x + RADIUS, y);
         CTX.arc(x, y, RADIUS, 0, PI_2);
     }
     CTX.fill();
     CTX.beginPath();
     for (var i = 1; i < M; ++i) {
-        var x = XS[i];
-        var y = YS[i];
+        var x = POINTS.x[i];
+        var y = POINTS.y[i];
         CTX.moveTo(x, y);
-        CTX.lineTo(x, y + (POINT_SPEED[i] * SCALE));
+        CTX.lineTo(x, y + (POINTS.speed[i] * L));
     }
     CTX.stroke();
     requestAnimationFrame(loop);
@@ -71,14 +73,14 @@ window.onload = function() {
     CTX.strokeStyle = COLOR;
     CTX.fillStyle = COLOR;
     CTX.lineWidth = 1;
-    HALF_HEIGHT = CANVAS.height / 2;
-    LOWER = CANVAS.height / 10;
+    HALF_HEIGHT = CANVAS.height / 2.0;
+    LOWER = CANVAS.height / 10.0;
     UPPER = CANVAS.height - LOWER;
     for (var i = 0; i < N; ++i) {
-        XS[i] = CANVAS.width * ((i + 0.5) / N);
-        YS[i] = HALF_HEIGHT;
-        POINT_SPEED[i] = 0;
-        AGGREGATE_SPEED[i] = 0;
+        POINTS.x[i] = CANVAS.width * ((i + 0.5) / N);
+        POINTS.y[i] = HALF_HEIGHT;
+        POINTS.speed[i] = 0.0;
+        POINTS.neighborSpeed[i] = 0.0;
     }
     loop();
 };
