46fa6083de74fe4a4c2388de9dd925e3eb5808cd
albertgoncalves <goncalves.albert@gmail.com>
2020-07-13 18:12:24 -0400

    re-factor

--- src/spatial_hash/script.js
+++ src/spatial_hash/script.js
@@ -1,13 +1,12 @@
 "use strict";
 
-var CANVAS, CTX, N, GRID, TABLE;
+var CANVAS, CTX, SIZE, J, I, N, GRID, TABLE;
 
-var J = 0;
-var I = 0;
-var M = 16;
 var T = 0;
 var RESET_MS = 5000;
+var M = 16;
 var CIRCLES = new Array(M);
+var INDICES = new Array(M);
 /* NOTE: Because the grid will capture `CIRCLES` indices in ascending order, we
  * can count on collision detection to occur only between ascending pairs of
  * indices. If the final object in `CIRCLES` collides with another other
@@ -20,12 +19,11 @@ var O = M - 1;
 var TESTED = new Array(O);
 var COLLISIONS = new Array(O);
 var PI_2 = Math.PI * 2.0;
-var SIZE = Math.floor((Math.random() * 50.0) + 50.0);
 var SCALE = 2.0;
 var HALF_SCALE = SCALE / 2.0;
 var RADIUS = 64.0;
-var RADIUS_2 = RADIUS * 2.0;
-var RADIUS_2_SQUARED = RADIUS_2 * RADIUS_2;
+var DIAMETER = RADIUS * 2.0;
+var DIAMETER_SQUARED = DIAMETER * DIAMETER;
 
 function numberToHex(x) {
     return x.toString(16).padStart(2, "0");
@@ -36,10 +34,44 @@ function colorToHex(color) {
         numberToHex(color.blue) + numberToHex(color.alpha);
 }
 
+function init() {
+    SIZE = Math.floor((Math.random() * 50.0) + 50.0);
+    J = Math.ceil(CANVAS.width / SIZE);
+    I = Math.ceil(CANVAS.height / SIZE);
+    N = J * I;
+    GRID = {
+        x: new Float32Array(N),
+        y: new Float32Array(N),
+    };
+    TABLE = new Array(N);
+    for (var i = 0; i < N; ++i) {
+        TABLE[i] = [];
+    }
+    var index = 0;
+    for (var y = 0; y < I; ++y) {
+        for (var x = 0; x < J; ++x) {
+            GRID.x[index] = x * SIZE;
+            GRID.y[index++] = y * SIZE;
+        }
+    }
+    for (var i = 0; i < M; ++i) {
+        CIRCLES[i] = {
+            x: Math.random() * CANVAS.width,
+            y: Math.random() * CANVAS.height,
+            color: colorToHex({
+                red: Math.floor(Math.random() * 256.0),
+                green: Math.floor(Math.random() * 256.0),
+                blue: Math.floor(Math.random() * 256.0),
+                alpha: 64,
+            }),
+            collided: false,
+        };
+        COLLISIONS[i] = [];
+    }
+}
+
 function getGridIndex(x, y) {
-    var j = Math.floor(x / SIZE);
-    var i = Math.floor(y / SIZE);
-    return j + (i * J);
+    return Math.floor(x / SIZE) + (Math.floor(y / SIZE) * J);
 }
 
 function circleToGridIndices(circle) {
@@ -68,75 +100,41 @@ function getDistanceSquared(a, b) {
     return (x * x) + (y * y);
 }
 
-function init() {
-    for (var i = 0; i < N; ++i) {
-        TABLE[i] = [];
-    }
-    var index = 0;
-    for (var y = 0; y < I; ++y) {
-        for (var x = 0; x < J; ++x) {
-            GRID.x[index] = x * SIZE;
-            GRID.y[index++] = y * SIZE;
-        }
-    }
-    for (var i = 0; i < M; ++i) {
-        CIRCLES[i] = {
-            x: Math.random() * CANVAS.width,
-            y: Math.random() * CANVAS.height,
-            color: colorToHex({
-                red: Math.floor(Math.random() * 256.0),
-                green: Math.floor(Math.random() * 256.0),
-                blue: Math.floor(Math.random() * 256.0),
-                alpha: 64,
-            }),
-            collided: false,
-        };
-        COLLISIONS[i] = [];
-    }
-}
-
-function testIndices(a, b) {
-    var tested = TESTED[a];
-    for (var l = tested.length - 1; 0 <= l; --l) {
-        if (b === tested[l]) {
+function testIndices(i, j) {
+    var tested = TESTED[i];
+    for (var k = tested.length - 1; 0 <= k; --k) {
+        if (j === tested[k]) {
             return;
         }
     }
-    var distanceSquared = getDistanceSquared(CIRCLES[a], CIRCLES[b]);
-    if (distanceSquared < RADIUS_2_SQUARED) {
-        COLLISIONS[a].push(b);
+    var a = CIRCLES[i];
+    var b = CIRCLES[j];
+    if (getDistanceSquared(a, b) < DIAMETER_SQUARED) {
+        COLLISIONS[i].push(j);
+        a.collided = true;
+        b.collided = true;
     }
-    tested.push(b);
+    tested.push(j);
 }
 
-function loop(t) {
-    if (RESET_MS < (t - T)) {
-        init();
-        T = t;
-    }
+function update() {
     for (var i = 0; i < N; ++i) {
         TABLE[i] = [];
+        if (i < O) {
+            TESTED[i] = [];
+            COLLISIONS[i] = [];
+        }
     }
     for (var i = 0; i < M; ++i) {
         var circle = CIRCLES[i];
         circle.x += (Math.random() * SCALE) - HALF_SCALE;
         circle.y += (Math.random() * SCALE) - HALF_SCALE;
         circle.collided = false;
-    }
-    for (var i = 0; i < O; ++i) {
-        TESTED[i] = [];
-        COLLISIONS[i] = [];
-    }
-    CTX.clearRect(0, 0, CANVAS.width, CANVAS.height);
-    for (var i = 0; i < M; ++i) {
-        var circle = CIRCLES[i];
         var indices = circleToGridIndices(circle);
         for (var j = indices.length - 1; 0 <= j; --j) {
-            var index = indices[j];
-            TABLE[index].push(i);
-            CTX.fillStyle = circle.color;
-            CTX.fillRect(GRID.x[index], GRID.y[index], SIZE, SIZE);
+            TABLE[indices[j]].push(i);
         }
+        INDICES[i] = indices;
     }
     for (var i = 0; i < N; ++i) {
         var indices = TABLE[i];
@@ -149,63 +147,72 @@ function loop(t) {
             }
         }
     }
-    {
-        CTX.lineWidth = 2;
-        CTX.setLineDash([]);
-        CTX.beginPath();
-        for (var i = 0; i < M; ++i) {
-            var circle = CIRCLES[i];
-            var collision = COLLISIONS[i];
-            for (var j = collision.length - 1; 0 <= j; --j) {
-                var a = CIRCLES[i];
-                var b = CIRCLES[collision[j]];
-                a.collided = true;
-                b.collided = true;
-                CTX.moveTo(a.x, a.y);
-                CTX.lineTo(b.x, b.y);
-            }
+}
+
+function draw() {
+    CTX.clearRect(0, 0, CANVAS.width, CANVAS.height);
+    /* NOTE: Grid cell colors via intersecting circles. */
+    for (var i = 0; i < M; ++i) {
+        var indices = INDICES[i];
+        for (var j = indices.length - 1; 0 <= j; --j) {
+            var index = indices[j];
+            CTX.fillStyle = CIRCLES[i].color;
+            CTX.fillRect(GRID.x[index], GRID.y[index], SIZE, SIZE);
         }
-        CTX.stroke();
     }
-    {
-        CTX.lineWidth = 1;
-        CTX.setLineDash([2, 8]);
-        CTX.beginPath();
-        for (var x = SIZE; x < CANVAS.width; x += SIZE) {
-            CTX.moveTo(x, 0);
-            CTX.lineTo(x, CANVAS.height);
-        }
-        for (var y = SIZE; y < CANVAS.height; y += SIZE) {
-            CTX.moveTo(0, y);
-            CTX.lineTo(CANVAS.width, y);
-        }
-        CTX.stroke();
+    /* NOTE: Grid lines. */
+    CTX.lineWidth = 1;
+    CTX.setLineDash([2, 8]);
+    CTX.beginPath();
+    for (var x = SIZE; x < CANVAS.width; x += SIZE) {
+        CTX.moveTo(x, 0);
+        CTX.lineTo(x, CANVAS.height);
     }
-    {
-        CTX.lineWidth = 2;
-        CTX.setLineDash([]);
-        CTX.beginPath();
-        for (var i = 0; i < M; ++i) {
-            var circle = CIRCLES[i];
-            if (!circle.collided) {
-                CTX.moveTo(circle.x + RADIUS, circle.y);
-                CTX.arc(circle.x, circle.y, RADIUS, 0.0, PI_2);
-            }
-        }
-        CTX.stroke();
+    for (var y = SIZE; y < CANVAS.height; y += SIZE) {
+        CTX.moveTo(0, y);
+        CTX.lineTo(CANVAS.width, y);
     }
-    {
-        CTX.setLineDash([2, 4]);
-        CTX.beginPath();
-        for (var i = 0; i < M; ++i) {
-            var circle = CIRCLES[i];
-            if (circle.collided) {
-                CTX.moveTo(circle.x + RADIUS, circle.y);
-                CTX.arc(circle.x, circle.y, RADIUS, 0.0, PI_2);
-            }
+    CTX.stroke();
+    /* NOTE: Collided circles with dashed perimeter. */
+    CTX.lineWidth = 2;
+    CTX.setLineDash([2, 4]);
+    CTX.beginPath();
+    for (var i = 0; i < M; ++i) {
+        if (CIRCLES[i].collided) {
+            CTX.moveTo(CIRCLES[i].x + RADIUS, CIRCLES[i].y);
+            CTX.arc(CIRCLES[i].x, CIRCLES[i].y, RADIUS, 0.0, PI_2);
         }
-        CTX.stroke();
     }
+    CTX.stroke();
+    /* NOTE: Un-collided circles with solid perimeter. */
+    CTX.setLineDash([]);
+    CTX.beginPath();
+    for (var i = 0; i < M; ++i) {
+        if (!CIRCLES[i].collided) {
+            CTX.moveTo(CIRCLES[i].x + RADIUS, CIRCLES[i].y);
+            CTX.arc(CIRCLES[i].x, CIRCLES[i].y, RADIUS, 0.0, PI_2);
+        }
+    }
+    /* NOTE: Center-to-center lines for colliding circles. */
+    for (var i = 0; i < M; ++i) {
+        var collision = COLLISIONS[i];
+        for (var j = collision.length - 1; 0 <= j; --j) {
+            var a = CIRCLES[i];
+            var b = CIRCLES[collision[j]];
+            CTX.moveTo(a.x, a.y);
+            CTX.lineTo(b.x, b.y);
+        }
+    }
+    CTX.stroke();
+}
+
+function loop(t) {
+    if (RESET_MS < (t - T)) {
+        init();
+        T = t;
+    }
+    update();
+    draw();
     requestAnimationFrame(loop);
 }
 
@@ -214,20 +221,6 @@ window.onload = function() {
     CTX = CANVAS.getContext("2d");
     CTX.imageSmoothingEnabled = false;
     CTX.strokeStyle = "hsl(0, 0%, 20%)";
-    {
-        for (var x = 0; x < CANVAS.width; x += SIZE) {
-            ++J;
-        }
-        for (var y = 0; y < CANVAS.height; y += SIZE) {
-            ++I;
-        }
-        N = J * I;
-        GRID = {
-            x: new Float32Array(N),
-            y: new Float32Array(N),
-        };
-        TABLE = new Array(N);
-    }
     init();
     loop(0);
 };
