837b400c4534c604f911b48d7899c1a85a4f228f
Albert Goncalves <goncalves.albert@gmail.com>
2020-07-22 00:53:10 -0400

    add some jitter

--- src/webgl_hello_world/script.js
+++ src/webgl_hello_world/script.js
@@ -4,52 +4,80 @@
  * `https://avikdas.com/2020/07/08/barebones-webgl-in-75-lines-of-code.html`
  */
 
+var GL;
+
+var DATA = new Float32Array([
+    -0.85, //     <- `x`
+    -0.85, //  1  <- `y`
+    0.0,   // ___ <- ... `opacity` ?
+    0.85,  //
+    -0.85, //  2
+    0.0,   // ___
+    0.0,   //
+    0.85,  //  3
+    0.0,   // ___
+]);
+var SCALE = 0.0035;
+var HALF_SCALE = SCALE / 2.0;
+
+function getSource(id) {
+    return document.getElementById(id).innerHTML;
+}
+
+function compileShader(shader, source) {
+    GL.shaderSource(shader, source);
+    GL.compileShader(shader);
+    if (!GL.getShaderParameter(shader, GL.COMPILE_STATUS)) {
+        console.error(GL.getShaderInfoLog(shader));
+        return false;
+    }
+    return true;
+}
+
+function update() {
+    DATA[0] += (Math.random() * SCALE) - HALF_SCALE;
+    DATA[1] += (Math.random() * SCALE) - HALF_SCALE;
+    DATA[3] += (Math.random() * SCALE) - HALF_SCALE;
+    DATA[4] += (Math.random() * SCALE) - HALF_SCALE;
+    DATA[6] += (Math.random() * SCALE) - HALF_SCALE;
+    DATA[7] += (Math.random() * SCALE) - HALF_SCALE;
+}
+
+function draw() {
+    GL.bufferData(GL.ARRAY_BUFFER, DATA, GL.STATIC_DRAW);
+    GL.clear(GL.COLOR_BUFFER_BIT);
+    GL.drawArrays(GL.TRIANGLES, 0, 3);
+}
+
 window.onload = function() {
     var canvas = document.getElementById("canvas");
-    var gl = canvas.getContext("webgl");
-    gl.clearColor(0.9, 0.9, 0.9, 1);
-    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertexShader,
-                    document.getElementById("vertex-shader").innerHTML);
-    gl.compileShader(vertexShader);
-    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-        console.error(gl.getShaderInfoLog(vertexShader));
+    GL = canvas.getContext("webgl");
+    var vertexShader = GL.createShader(GL.VERTEX_SHADER);
+    if (!compileShader(vertexShader, getSource("vertex-shader"))) {
         return;
     }
-    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragmentShader,
-                    document.getElementById("fragment-shader").innerHTML);
-    gl.compileShader(fragmentShader);
-    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-        console.error(gl.getShaderInfoLog(fragmentShader));
+    var fragmentShader = GL.createShader(GL.FRAGMENT_SHADER);
+    if (!compileShader(fragmentShader, getSource("fragment-shader"))) {
         return;
     }
-    var program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-        console.error(gl.getProgramInfoLog(program));
+    var program = GL.createProgram();
+    GL.attachShader(program, vertexShader);
+    GL.attachShader(program, fragmentShader);
+    GL.linkProgram(program);
+    if (!GL.getProgramParameter(program, GL.LINK_STATUS)) {
+        console.error(GL.getProgramInfoLog(program));
         return;
     }
-    gl.useProgram(program);
-    var data = new Float32Array([
-        -0.85, //     <- `x`
-        -0.85, //  1  <- `y`
-        0.0,   // ___ <- ... `opacity` ?
-        0.85,  //
-        -0.85, //  2
-        0.0,   // ___
-        0.0,   //
-        0.85,  //  3
-        0.0,   // ___
-    ]);
-    var buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
-    var attribute = gl.getAttribLocation(program, "position");
-    gl.enableVertexAttribArray(attribute);
-    gl.vertexAttribPointer(attribute, 3, gl.FLOAT, false, 0, 0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
+    GL.useProgram(program);
+    GL.bindBuffer(GL.ARRAY_BUFFER, GL.createBuffer());
+    var attribute = GL.getAttribLocation(program, "position");
+    GL.enableVertexAttribArray(attribute);
+    GL.clearColor(0.9, 0.9, 0.9, 1);
+    GL.vertexAttribPointer(attribute, 3, GL.FLOAT, false, 0, 0);
+    function loop() {
+        update();
+        draw();
+        requestAnimationFrame(loop);
+    }
+    requestAnimationFrame(loop);
 };
