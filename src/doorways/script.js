e6aba4693754760f68025c79ff41a0d361fdd5da
albertgoncalves <goncalves.albert@gmail.com>
2023-07-27 22:40:29 -0400

    big revision, more hard stuff still to go

--- src/doorways/script.js
+++ src/doorways/script.js
@@ -4,141 +4,208 @@ var TAU = Math.PI * 2;
 
 var COLOR = "hsl(0, 0%, 35%)";
 
-var LINE_WIDTH = 2.25;
-var POINT_RADIUS = 3;
+var LINE_WIDTH = 3.25;
+var LINE_CAP = "square";
 
-function Room(x, y, w, h, k) {
-    var f = function(c, s, k) {
-        return c + (s * ((Math.random() * (1 - k)) + (k / 2)));
+var POINT_RADIUS = 4;
+
+var BUFFER = 80;
+var K = 3;
+
+var GAP = 50;
+
+var INTERVAL = 1750;
+
+var X = 50;
+var Y = 50;
+
+if (BUFFER <= GAP) {
+    throw new Error();
+}
+
+function split(lines, x, y, w, h, flag) {
+    if (Math.min(w, h) <= (BUFFER * K)) {
+        return;
+    }
+
+    if (flag) {
+        var k = Math.min(Math.max(Math.random() * w, BUFFER), w - BUFFER);
+        var xK = x + k;
+
+        lines.push([[xK, y], [xK, y + h]]);
+        split(lines, x, y, k, h, false);
+        split(lines, xK, y, w - k, h, false);
+    } else {
+        var k = Math.min(Math.max(Math.random() * h, BUFFER), h - BUFFER);
+        var yK = y + k;
+
+        lines.push([[x, yK], [x + w, yK]]);
+        split(lines, x, y, w, k, true);
+        split(lines, x, yK, w, h - k, true);
+    }
+}
+
+function setIfNotFound(object, key, value) {
+    if (object.hasOwnProperty(key)) {
+        return;
+    }
+    object[key] = value;
+}
+
+function pushIfNotFound(array, element) {
+    if (0 <= array.indexOf(element)) {
+        return;
+    }
+    array.push(element);
+}
+
+function lerp(a, b, t) {
+    return a + ((b - a) * t);
+}
+
+function len(x0, y0, x1, y1) {
+    var x2 = x1 - x0;
+    var y2 = y1 - y0;
+    return Math.sqrt((x2 * x2) + (y2 * y2));
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function generate(canvas) {
+    var lines = [];
+    split(lines, X, Y, canvas.width - (X * 2), canvas.height - (Y * 2), true);
+
+    var xs = {};
+    var ys = {};
+    for (var i = 0; i < lines.length; ++i) {
+        var x0 = lines[i][0][0];
+        var y0 = lines[i][0][1];
+        var x1 = lines[i][1][0];
+        var y1 = lines[i][1][1];
+
+        setIfNotFound(xs, x0, []);
+        setIfNotFound(xs, x1, []);
+        setIfNotFound(ys, y0, []);
+        setIfNotFound(ys, y1, []);
+
+        pushIfNotFound(xs[x0], y0);
+        pushIfNotFound(xs[x0], y1);
+        pushIfNotFound(xs[x1], y0);
+        pushIfNotFound(xs[x1], y1);
+        pushIfNotFound(ys[y0], x0);
+        pushIfNotFound(ys[y0], x1);
+        pushIfNotFound(ys[y1], x0);
+        pushIfNotFound(ys[y1], x1);
+    }
+
+    for (var i = 0; i < Object.keys(xs).length; ++i) {
+        xs[Object.keys(xs)[i]].sort(subtract);
+    }
+    for (var i = 0; i < Object.keys(ys).length; ++i) {
+        ys[Object.keys(ys)[i]].sort(subtract);
+    }
+
+    lines = [];
+    var vs, n;
+    for (var i = 0; i < Object.keys(xs).length; ++i) {
+        vs = xs[Object.keys(xs)[i]];
+        n = vs.length - 1;
+        if (n === 0) {
+            continue;
+        }
+        var x = Number(Object.keys(xs)[i]);
+        for (var j = 0; j < n; ++j) {
+            lines.push([[x, vs[j]], [x, vs[j + 1]]]);
+        }
+    }
+    for (var i = 0; i < Object.keys(ys).length; ++i) {
+        vs = ys[Object.keys(ys)[i]];
+        n = vs.length - 1;
+        if (n === 0) {
+            continue;
+        }
+        var y = Number(Object.keys(ys)[i]);
+        for (var j = 0; j < n; ++j) {
+            lines.push([[vs[j], y], [vs[j + 1], y]]);
+        }
+    }
+
+    var state = {
+        lines: [],
+        points: [],
     };
+    for (var i = 0; i < lines.length; ++i) {
+        var x0 = lines[i][0][0];
+        var y0 = lines[i][0][1];
+        var x1 = lines[i][1][0];
+        var y1 = lines[i][1][1];
 
-    var kW = k / w;
-    var kH = k / h;
+        var l = len(x0, y0, x1, y1);
 
-    var kS = k / 2;
-
-    var x0 = f(x, w, kW);
-    var x1 = f(x, w, kW);
-
-    var y0 = f(y, h, kH);
-    var y1 = f(y, h, kH);
-
-    var xW = x + w;
-    var yH = y + h;
-
-    this.nodes = [
-        [x, y],       // 0
-        [x0 - kS, y], //
-        [x0, y],      //
-        [x0 + kS, y], //
-
-        [xW, y],       // 4
-        [xW, y0 - kS], //
-        [xW, y0],      //
-        [xW, y0 + kS], //
-
-        [xW, yH],      // 8
-        [x1 + kS, yH], //
-        [x1, yH],      //
-        [x1 - kS, yH], //
-
-        [x, yH],      // 12
-        [x, y1 + kS], //
-        [x, y1],      //
-        [x, y1 - kS], //
-
-        [x0, y0], // 16
-        [x0, y1], //
-        [x1, y1], //
-        [x1, y0], //
-    ];
-    this.edges = [
-        [0, 1],
-        [3, 4],
-        //
-        [4, 5],
-        [7, 8],
-        //
-        [8, 9],
-        [11, 12],
-        //
-        [12, 13],
-        [15, 0],
-        //
-        [16, 17],
-        [17, 18],
-        [18, 19],
-        [19, 16],
-    ];
-
-    if (y0 < y1) {
-        this.edges.push([2, 16]);
-        this.edges.push([10, 18]);
-    } else {
-        this.edges.push([2, 17]);
-        this.edges.push([10, 19]);
-    }
-
-    if (x0 < x1) {
-        this.edges.push([6, 19]);
-        this.edges.push([14, 17]);
-    } else {
-        this.edges.push([6, 16]);
-        this.edges.push([14, 18]);
-    }
-}
-
-function update(rooms) {
-    for (var i = 0; i < rooms.length; ++i) {
-        for (var j = 0; j < rooms[i].nodes.length; ++j) {
-            rooms[i].nodes[j][0] += (Math.random() * 0.5) - 0.25;
-            rooms[i].nodes[j][1] += (Math.random() * 0.5) - 0.25;
+        if (l < (BUFFER * 2)) {
+            state.lines.push(lines[i]);
+            continue;
         }
+
+        var k = BUFFER / l;
+        var t = ((1 - k) * Math.random()) + (k / 2);
+
+        state.points.push([lerp(x0, x1, t), lerp(y0, y1, t)]);
+
+        var m = GAP / l;
+
+        state.lines.push(
+            [[x0, y0], [lerp(x0, x1, t - m), lerp(y0, y1, t - m)]]);
+        state.lines.push(
+            [[lerp(x0, x1, t + m), lerp(y0, y1, t + m)], [x1, y1]]);
     }
+    return state;
 }
 
-function draw(canvas, context, rooms) {
+function draw(canvas, context, state) {
     context.clearRect(0, 0, canvas.width, canvas.height);
+
     context.beginPath();
-
-    for (var i = 0; i < rooms.length; ++i) {
-        for (var j = 0; j < rooms[i].nodes.length; ++j) {
-            context.moveTo(rooms[i].nodes[j][0], rooms[i].nodes[j][1]);
-            context.arc(rooms[i].nodes[j][0],
-                        rooms[i].nodes[j][1],
-                        POINT_RADIUS,
-                        0,
-                        TAU);
-        }
-    }
-    context.fill();
-
-    for (var i = 0; i < rooms.length; ++i) {
-        for (var j = 0; j < rooms[i].edges.length; ++j) {
-            var a = rooms[i].nodes[rooms[i].edges[j][0]];
-            var b = rooms[i].nodes[rooms[i].edges[j][1]];
-            context.moveTo(a[0], a[1]);
-            context.lineTo(b[0], b[1]);
-        }
+    for (var i = 0; i < state.lines.length; ++i) {
+        context.moveTo(state.lines[i][0][0], state.lines[i][0][1]);
+        context.lineTo(state.lines[i][1][0], state.lines[i][1][1]);
     }
     context.stroke();
+
+    context.beginPath();
+    for (var i = 0; i < state.points.length; ++i) {
+        context.moveTo(state.points[i][0], state.points[i][1]);
+        context.arc(state.points[i][0],
+                    state.points[i][1],
+                    POINT_RADIUS,
+                    0,
+                    TAU);
+    }
+    context.fill();
 }
 
 window.onload = function() {
     var canvas = document.getElementById("canvas");
+    if (canvas === null) {
+        throw new Error();
+    }
     var context = canvas.getContext("2d");
     context.imageSmoothingEnabled = false;
+    context.lineWidth = LINE_WIDTH;
+    context.lineCap = LINE_CAP;
     context.strokeStyle = COLOR;
     context.fillStyle = COLOR;
-    context.lineWidth = LINE_WIDTH;
 
-    var rooms = [
-        new Room(50, 50, 800, 450, 150),
-    ];
+    draw(canvas, context, generate(canvas));
 
+    var prev = performance.now();
     var loop = function(now) {
-        update(rooms);
-        draw(canvas, context, rooms);
+        if (INTERVAL <= (now - prev)) {
+            draw(canvas, context, generate(canvas));
+            prev = now;
+        }
         window.requestAnimationFrame(loop);
     };
     window.requestAnimationFrame(loop);
